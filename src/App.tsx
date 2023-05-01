a0d31fd75e93689fc3ab2c8e0e7ec29245fe8ae5
Marq W <marquisewest86@duck.com>
2023-04-30 23:13:13 -0500

    css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import Button from "./components/Buttons/Buttons.module.css";
-import styles from "./components/ListGroup/ListGroup.module.css"
-
 function App() {
 
   return (
-    <div>
-      <Button className="{styles.Button}" onClick={() => {console.log('clicked')}}>My button</Button>
-    </div>  
+    <button className="btn btn-primary">My Button!</button> 
   )
   
 }
--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,26 +1,15 @@
-// interface Props {
-//   children: string;
-//   color?:
-//     | "primary"
-//     | "secondary"
-//     | "danger"
-//     | "warning"
-//     | "info"
-//     | "light"
-//     | "dark";
-//   onClick: () => void;
-// }
+
+import styles from './Buttons/Buttons.module.css';
 
 interface Props {
-    children: string;
-    onClick: () => void;
+  children: string;
+  color?: "primary";
+  onClick: () => void;
 }
 
-const Button = ({children, onClick}: Props) => {
+const Button = ({ children, onClick, color }: Props) => {
   return (
-    <div>
-        <button className="stinkyButton" onClick={onClick}>{children}</button>
-    </div>
+    <button className={[styles.btn, styles['btn-' + color]].join(' ')} onClick={onClick}>{children}</button>
   );
 };
 
